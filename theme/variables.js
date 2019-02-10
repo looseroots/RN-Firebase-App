f8b96fde7b4fcd41748fee3f0fff152abe698980
Cody Swain <swain.cody@gmail.com>
2019-02-09 20:03:20 -0800

    Green Color Scheme

--- theme/variables.js
+++ theme/variables.js
@@ -1,7 +1,10 @@
 export const colors = {
-  THEME_COLOR: "#FF9900",
-  // THEME_COLOR: "#219653",
-  // THEME_COLOR: "#115B94",
+  // THEME_COLOR: "#FF9900", 			// Orange
+  THEME_COLOR: "#27AE60", 			// Green
+  // THEME_COLOR: "#9B51E0",			// Purple
+  // THEME_COLOR: "#EB5757",			// Red
+  // THEME_COLOR: "#115B94",			// Dark blue
+  // THEME_COLOR: "#F2C94C",			//Yellow
 };
 
 export const fonts = {
